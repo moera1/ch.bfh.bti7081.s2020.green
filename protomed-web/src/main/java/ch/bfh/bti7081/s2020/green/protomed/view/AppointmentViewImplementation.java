56d5c4bcf0168e85541de1b0702b9a2bad43c1cb
Amalathas Kevin <amalathask@zeami.ch>
2020-05-11 21:53:07 +0200

    feat: Adjusted AppointmentView

--- protomed-web/src/main/java/ch/bfh/bti7081/s2020/green/protomed/view/AppointmentViewImplementation.java
+++ protomed-web/src/main/java/ch/bfh/bti7081/s2020/green/protomed/view/AppointmentViewImplementation.java
@@ -1,5 +1,14 @@
 package ch.bfh.bti7081.s2020.green.protomed.view;
 
+import com.github.appreciated.card.Card;
+import com.github.appreciated.card.ClickableCard;
+import com.github.appreciated.card.label.TitleLabel;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import java.util.ArrayList;
@@ -11,7 +20,46 @@ public class AppointmentViewImplementation extends VerticalLayout implements App
 
     public AppointmentViewImplementation() {
 
-        addClassName("appointment-container");
+        add(new H2("Nächste Termine"));
+
+        ClickableCard card1 = new ClickableCard(
+                onClick -> {
+                    Notification.show("Card is clicked");
+                },
+                new Label("12. Mai"),
+                new Icon(VaadinIcon.VAADIN_H),
+                new Label("11:00 - Alexandra Vonesch"),
+                new Label("Bernstrasse 107, 3007 Bern")
+        );
+
+        ClickableCard card2 = new ClickableCard(
+                onClick -> {
+                    Notification.show("Card is clicked");
+                },
+                new Label("12. Mai"),
+                new Icon(VaadinIcon.VAADIN_H),
+                new Label("14:00 - John Abara"),
+                new Label("Bielstrasse 42, 3011 Bern")
+        );
+
+
+        ClickableCard card3 = new ClickableCard(
+                onClick -> {
+                    Notification.show("Card is clicked");
+                },
+                new Label("12. Mai"),
+                new Icon(VaadinIcon.VAADIN_H),
+                new Label("09:00 - Michael Müller"),
+                new Label("Wankdorfstrasse 102, 3014 Bern")
+        );
+
+        card1.setWidth("100%");
+        card2.setWidth("100%");
+        card3.setWidth("100%");
+
+        add(card1);
+        add(card2);
+        add(card3);
     }
 
     @Override
