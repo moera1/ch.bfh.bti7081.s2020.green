77cf3614ac9ce60c8e050dcdd346f0033a31c98a
Rindlisbacher <jose.rindlisbacher@gmail.com>
2020-05-18 20:19:43 +0200

    removed leftover references.. - sorry

--- protomed-web/src/main/java/ch/bfh/bti7081/s2020/green/protomed/management/mock/ProtocolMock.java
+++ protomed-web/src/main/java/ch/bfh/bti7081/s2020/green/protomed/management/mock/ProtocolMock.java
@@ -1,6 +1,6 @@
 package ch.bfh.bti7081.s2020.green.protomed.management.mock;
 
-import ch.bfh.bti7081.s2020.green.protomed.management.AppointmentManager;
+import ch.bfh.bti7081.s2020.green.protomed.management.ApplicationModelManager;
 import ch.bfh.bti7081.s2020.green.protomed.management.HealthClientManager;
 import ch.bfh.bti7081.s2020.green.protomed.management.HealthVisitorManager;
 import ch.bfh.bti7081.s2020.green.protomed.model.*;
@@ -25,7 +25,7 @@ public class ProtocolMock {
         HealthClient lars = HealthClientManager.getInstance().getHealthClient(3);
 
         // Protocols of Laura
-        List<Appointment> lauraAppointments = AppointmentManager.getInstance().getAppointmentsByHealthVisitorID(laura.getPersonId());
+        List<Appointment> lauraAppointments = ApplicationModelManager.getInstance().getAppointmentsByHealthVisitorID(laura.getPersonId());
 
         protocols.add(new Protocol(laura, daniel, time, ProtocolType.NOTIZ, "This is a test!"));
         protocols.add(new BesuchProtocol(laura, daniel, time, lauraAppointments.get(0), "This is the first protocol."));
@@ -36,7 +36,7 @@ public class ProtocolMock {
         protocols.add(new Protocol(laura, daniel, time, ProtocolType.TELEFON));
 
         // Protocols of Gertrud
-        List<Appointment> gertrudAppointments = AppointmentManager.getInstance().getAppointmentsByHealthVisitorID(gertrud.getPersonId());
+        List<Appointment> gertrudAppointments = ApplicationModelManager.getInstance().getAppointmentsByHealthVisitorID(gertrud.getPersonId());
 
         protocols.add(new BesuchProtocol(gertrud, lars, time, gertrudAppointments.get(0), "This is the first protocol."));
         protocols.add(new BesuchProtocol(gertrud, lars, time, gertrudAppointments.get(1), "This is the second protocol."));
@@ -46,7 +46,7 @@ public class ProtocolMock {
 
 
         // Protocols of Max
-        List<Appointment> maxAppointments = AppointmentManager.getInstance().getAppointmentsByHealthVisitorID(max.getPersonId());
+        List<Appointment> maxAppointments = ApplicationModelManager.getInstance().getAppointmentsByHealthVisitorID(max.getPersonId());
 
         protocols.add(new BesuchProtocol(max, thomas, time, maxAppointments.get(0), "This is the first protocol."));
         protocols.add(new BesuchProtocol(max, thomas, time, maxAppointments.get(1), "This is an example."));
