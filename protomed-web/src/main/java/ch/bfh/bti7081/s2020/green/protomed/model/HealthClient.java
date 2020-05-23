798f4ad45704f0b6f7f5dbd249ed4c0f5ae35671
MJod <markus24@bluewin.ch>
2020-05-23 11:17:17 +0200

    Fix broken HealthVisitor model

--- protomed-web/src/main/java/ch/bfh/bti7081/s2020/green/protomed/model/HealthClient.java
+++ protomed-web/src/main/java/ch/bfh/bti7081/s2020/green/protomed/model/HealthClient.java
@@ -1,14 +1,20 @@
 package ch.bfh.bti7081.s2020.green.protomed.model;
 
 
+import lombok.Getter;
+
 import java.time.LocalDate;
+import java.util.List;
 
 public class HealthClient extends Person {
 
     private HealthVisitor healthVisitor;
+    @Getter
+    private List<HealthService> insuredServices;
 
-    public HealthClient(HealthVisitor healthVisitor, int personId, Address address, String name, String firstname, LocalDate birthDate, String phoneNumber, String profilePicture) {
+    public HealthClient(HealthVisitor healthVisitor, List<HealthService> insuredServices, int personId, Address address, String name, String firstname, LocalDate birthDate, String phoneNumber, String profilePicture) {
         super(personId, address, name, firstname, birthDate, phoneNumber, profilePicture);
         this.healthVisitor = healthVisitor;
+        this.insuredServices = insuredServices;
     }
 }
